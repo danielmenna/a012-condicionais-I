cae73cb6d9f877a394772a966b74bc688fcd326d
danielmenna <danielmenna@gmail.com>
2022-08-04 13:30:37 -0300

    exercicios do dia

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,14 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt('Digite a sua nacionalidade:') //= adicione o prompt aqui
+if (nacionalidade === 'brasileira') {
+  console.log('Você é brasileiro')
+} else if (nacionalidade === 'argentina') {
+  console.log('Você é argentino')
+} else if (nacionalidade === 'uruguaia') {
+  console.log('Você é uruguaio')
+} else if (nacionalidade === 'chilena') {
+  console.log('Você é chileno')
+} else if (nacionalidade === 'colombiana') {
+  console.log('Você é colombiano')
+} else {
+  console.log('Nacionalidade não encontrada')
+}
